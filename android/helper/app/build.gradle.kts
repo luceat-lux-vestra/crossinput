061249e19bb665df0a8fb2aa377bc197c23a48c8
luceat-lux-vestra <heathkimdev@gmail.com>
2026-08-29 10:06:22 +0900

    build(android): migrate Kotlin compiler options

--- android/helper/app/build.gradle.kts
+++ android/helper/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -20,10 +22,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildTypes {
         debug {
             // debug build for app_process execution (release avoids path hardcoding issues)
@@ -32,6 +30,12 @@ android {
 
     testOptions {
         unitTests.isReturnDefaultValues = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
     }
 }
 
