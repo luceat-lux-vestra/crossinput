69b2349e85df849099a7464764ea3c2df60ef2d1
luceat-lux-vestra <heathkimdev@gmail.com>
2026-09-21 00:57:18 +0900

    feat(telemetry): classify boundary alignment requests

--- apps/macos/Sources/AndroidBridge/Telemetry.swift
+++ apps/macos/Sources/AndroidBridge/Telemetry.swift
@@ -20,6 +20,7 @@ public struct RequestObservation: Sendable, Equatable {
         case pointerMoveRel = "POINTER_MOVE_REL"
         case pointerButton = "POINTER_BUTTON"
         case pointerScroll = "POINTER_SCROLL"
+        case pointerAlignBoundary = "POINTER_ALIGN_BOUNDARY"
         case shutdown = "SHUTDOWN"
         case other = "OTHER"
         public init(of type: MessageType) {
@@ -31,6 +32,7 @@ public struct RequestObservation: Sendable, Equatable {
             case .pointerMoveRel: self = .pointerMoveRel
             case .pointerButton: self = .pointerButton
             case .pointerScroll: self = .pointerScroll
+            case .pointerAlignBoundary: self = .pointerAlignBoundary
             case .shutdown: self = .shutdown
             default: self = .other
             }
